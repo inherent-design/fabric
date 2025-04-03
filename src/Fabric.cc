b6a8054074c2cf737b3b44a77a24c36a5b47e87a
mannie-exe <manpreet.mannie.singh@gmail.com>
2025-04-02 18:20:40 -0700

    Attempt Windows fix

--- src/Fabric.cc
+++ src/Fabric.cc
@@ -21,6 +21,20 @@ void printHelp() {
 #include <windows.h>
 int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
                    LPSTR /*lpCmdLine*/, int /*nCmdShow*/) {
+  // Get command line arguments on Windows
+  int argc = 0;
+  LPWSTR *argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+  // Convert wide strings to regular strings
+  char **argv = new char *[argc];
+  for (int i = 0; i < argc; i++) {
+    int size = WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, nullptr, 0,
+                                   nullptr, nullptr);
+    argv[i] = new char[size];
+    WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, argv[i], size, nullptr,
+                        nullptr);
+  }
+  LocalFree(argvW);
 #else
 int main(int argc, char *argv[]) {
 #endif
@@ -42,12 +56,26 @@ int main(int argc, char *argv[]) {
     // Check for --help argument
     if (parser.getArgument("--help")) {
       printHelp();
+#if defined(_WIN32)
+      // Clean up allocated memory before returning
+      for (int i = 0; i < argc; i++) {
+        delete[] argv[i];
+      }
+      delete[] argv;
+#endif
       return 0;
     }
 
     // Check for --version argument
     if (parser.getArgument("--version")) {
       printVersion();
+#if defined(_WIN32)
+      // Clean up allocated memory before returning
+      for (int i = 0; i < argc; i++) {
+        delete[] argv[i];
+      }
+      delete[] argv;
+#endif
       return 0;
     }
 
@@ -55,6 +83,13 @@ int main(int argc, char *argv[]) {
     if (!parser.isValid()) {
       Logger::logError(parser.getErrorMsg());
       printHelp();
+#if defined(_WIN32)
+      // Clean up allocated memory before returning
+      for (int i = 0; i < argc; i++) {
+        delete[] argv[i];
+      }
+      delete[] argv;
+#endif
       return 1; // Exit with error code
     }
 
@@ -65,8 +100,22 @@ int main(int argc, char *argv[]) {
     w.run();
   } catch (const std::exception &e) {
     std::cerr << "Fatal error: " << e.what() << '\n';
+#if defined(_WIN32)
+    // Clean up allocated memory in case of exception
+    for (int i = 0; i < argc; i++) {
+      delete[] argv[i];
+    }
+    delete[] argv;
+#endif
     return 1;
   }
 
+#if defined(_WIN32)
+  // Clean up allocated memory before exiting
+  for (int i = 0; i < argc; i++) {
+    delete[] argv[i];
+  }
+  delete[] argv;
+#endif
   return 0;
 }
